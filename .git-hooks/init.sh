8fd2a7ac3846a795c0f15b0688a71332a2eb714b
Ahmed Elsadek <ahmed.elsadek@ekvip.com>
2026-08-05 11:40:34 +0200

    init

--- .git-hooks/init.sh
+++
@@ -1,9 +0,0 @@
-echo "Pulling all the remote machines ..."
-git branch -r | sed 's|origin/||'
-echo "Select the machine branch , Copy and paste it ..."
-read machine_branch
-
-git checkout main
-git pull origin machine_branch
-git checkout $machine_branch
-
